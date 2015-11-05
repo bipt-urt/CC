7af7fb3dd1c595b0abfdf071406fffe44f7d3ce0
DGideas <dgideas@outlook.com>
2015-11-05 11:18:53 +0800

    sync workflow

---
+++ web/haha.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>你好</title>
+		<meta charset="utf-8" />
+	</head>
+	<body>
+		<h1>你好</h1>
+		<p><?php
+			$a="hello";
+			var_dump($GLOBALS);
+		?>
+		</p>
+	</body>
+</html>
