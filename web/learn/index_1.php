227d66288b372f39c0d783ff0dbca6f459041815
DGideas <dgideas@outlook.com>
2015-11-09 22:48:22 +0800

    sync code:HTML user-interface

---
+++ web/learn/index_1.php
@@ -0,0 +1,10 @@
+<html>
+	<head> </head>
+		<body>
+<form action="hello.php" method="post">
+	What is your first name ?
+	<input type="test" name="firstname">
+	<input type="submit" value="Say Hello!"
+</form>
+		</body>
+		</body>	
